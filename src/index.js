3fcb6860d4f40cd80dbe1af8b5f938a5d5e5b311
Aidor777 <guillaume.garin@live.com>
2022-09-23 17:25:25 +0200

    First component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Hello from './hello/Hello';
+import 'tachyons';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Hello greeting={'Hello ' + 'React ' + 'Ninja'}/>
   </React.StrictMode>
 );
 

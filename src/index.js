c200ac6c46f4dd10d68f926ed6b91d2cee32fd35
Aidor777 <guillaume.garin@live.com>
2022-09-29 17:24:16 +0200

    Card and CardList components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Hello from './hello/Hello';
+import CardList from './cards/CardList';
 import 'tachyons';
 import reportWebVitals from './reportWebVitals';
+import {robots} from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hello greeting={'Hello ' + 'React ' + 'Ninja'}/>
+    <CardList robots={robots}/>
   </React.StrictMode>
 );
 
